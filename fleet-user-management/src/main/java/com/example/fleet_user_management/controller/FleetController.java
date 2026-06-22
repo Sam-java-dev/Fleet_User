becf8bb1e288155dc1b42177f5e81d615e90b21f
Sampath Kumar Karre <sampathkumarkarre25@gmail.com>
2026-06-22 17:58:39 +0530

    controller change

---
+++ fleet-user-management/src/main/java/com/example/fleet_user_management/controller/FleetController.java
@@ -0,0 +1,40 @@
+package com.example.fleet_user_management.controller;
+
+import com.example.fleet_user_management.dto.FleetRequest;
+import com.example.fleet_user_management.entity.Fleet;
+import com.example.fleet_user_management.service.FleetService;
+import jakarta.validation.Valid;
+import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/api/fleet")
+@RequiredArgsConstructor
+public class FleetController {
+    private final FleetService fleetService;
+
+    @PostMapping
+    public Fleet createFleet(@Valid @RequestBody FleetRequest request) {
+        return fleetService.createFleet(request);
+    }
+
+    @GetMapping("/{id}")
+    public Fleet getFleetById(@PathVariable String id) {
+        return fleetService.getFleetById(id);
+    }
+
+    @GetMapping
+    public List<Fleet> getAllFleets() {
+        return fleetService.getAllFleets();
+    }
+
+    @DeleteMapping
+    public Fleet deleteFleetById(@RequestParam String id) {
+        System.out.println("Deleting fleet with ID: " + id);
+        return fleetService.DeleteFleetById(id);
+    }
+
+
+}
